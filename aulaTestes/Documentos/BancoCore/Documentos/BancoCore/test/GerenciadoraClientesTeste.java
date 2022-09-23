230fcce6651490314e6ff9efcaf9cd6abb366618
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-09-23 19:59:30 -0300

    cenário de teste do método RemoveCliente

--- aulaTestes/Documentos/BancoCore/Documentos/BancoCore/test/GerenciadoraClientesTeste.java
+++ aulaTestes/Documentos/BancoCore/Documentos/BancoCore/test/GerenciadoraClientesTeste.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 import sistemabancario.Cliente;
@@ -10,6 +11,32 @@ import sistemabancario.GerenciadoraClientes;
 
 public class GerenciadoraClientesTeste {
 
+	
+	@Test
+	public void testeRemoveCliente() {
+		
+		//montagem do cenário de teste
+		Cliente cliente01 = new Cliente(1, "Clayton", 47, "clayton@gmail.com", 1 ,true);
+		Cliente cliente02 = new Cliente(2, "Maria", 10, "maria@gmail.com", 1 ,true);
+		
+		List<Cliente> clientes = new ArrayList<>();
+		clientes.add(cliente01);
+		clientes.add(cliente02);
+		
+		GerenciadoraClientes gerClientes = new GerenciadoraClientes(clientes);
+		
+		//******************************************//
+		//fase de execução do teste
+		boolean clienteRemovido = gerClientes.removeCliente(2);
+		
+		//análise do resultado esperado
+		assertThat(clienteRemovido, is(true));
+		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
+		assertNull(gerClientes.pesquisaCliente(2));
+	}
+	
+	
+	
 	@Test
 	public void testePesquisaCliente() {
 		/*criando clientes para instaciar no caso de Teste*/
@@ -37,4 +64,8 @@ public class GerenciadoraClientesTeste {
 		assertThat(outroCliente.getNome(), is("Maria"));
 		assertThat(outroCliente.getIdade(), is(10));
 	}
+	
+	
+	
+	
 }
